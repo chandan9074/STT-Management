60f8006b2679f359fab9c9de411fdd3ee982a097
jalal.seu.cse <jalal.seu.cse@gmail.com>
2022-12-04 15:19:36 +0600

    router added

---
+++ src/components/TimeWiseDisbursement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TimeWiseDisbursement = () => {
+    return (
+        <div>
+            time wise disbursement
+        </div>
+    );
+};
+
+export default TimeWiseDisbursement;
