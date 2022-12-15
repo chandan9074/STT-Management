3d2e9c1c00f1942d2da3c8e846c0ee692dcca913
raihan-ops <tasnimraihan28@gmail.com>
2022-12-15 13:21:09 +0600

    fixed table design

--- src/components/containers/billing/BillingList/BillingListIndex.tsx
+++ src/components/containers/billing/BillingList/BillingListIndex.tsx
@@ -1,7 +1,113 @@
 import React from 'react';
 import BillingTable from "./BillingTable";
+import {InputNumber} from "antd";
+import Primary from "./Primary";
+import {ColumnsType} from "antd/es/table";
+import ManagerIcon from "../../../../assets/images/BillingManagerAvatar.png"
 
+interface Person {
+    title: string;
+    subTitle: string;
+}
+
+export interface BillingDataType {
+    key: React.Key;
+    manager: Person
+    hour: string;
+    amountPaid: string;
+}
+
+export interface AllBillingDataType {
+    key: React.Key;
+    date: any
+    hour: string;
+    amountPaid: string;
+}
+
+const lastBillingColumns: ColumnsType<BillingDataType | AllBillingDataType> = [
+    {
+        title: 'MANAGER',
+        dataIndex: 'manager',
+        width: 100,
+        render: (data) => (
+            <>
+                <div className="flex gap-2 items-start">
+                    <img className="h-4 w-4 mt-1" src={ManagerIcon} alt=""/>
+                    <div>
+                        <p className="font-sans font-medium text-xs">{data.title}</p>
+                        <p className="font-sans text-xxs text-blue-gray-75">{data.subTitle}</p>
+                    </div>
+                </div>
+            </>
+        )
+
+    },
+    {
+        title: 'HOUR ',
+        dataIndex: 'hour',
+        width: 100,
+
+
+    },
+    {
+        title: 'AMOUNT PAID',
+        dataIndex: 'amountPaid',
+        width: 100,
+
+    },
+];
+const allBillingColumns: ColumnsType<AllBillingDataType | BillingDataType> = [
+    {
+        title: 'DATE',
+        dataIndex: 'date',
+        width: 100,
+
+
+    },
+    {
+        title: 'HOUR ',
+        dataIndex: 'hour',
+        width: 100,
+
+
+    },
+    {
+        title: 'AMOUNT PAID',
+        dataIndex: 'amountPaid',
+        width: 100,
+
+    },
+];
+
+const data: BillingDataType[] = [];
+
+for (let i = 0; i < 8; i++) {
+    data.push({
+        key: i,
+        manager: {
+            title: `Edward King ${i}`,
+            subTitle: `sub-title ${i}`,
+
+        },
+        hour: `${i} hr`,
+        amountPaid: ` ${i} /-`,
+    });
+
+
+}
+const anotherData: AllBillingDataType[] = []
+for (let i = 0; i < 8; i++) {
+    anotherData.push({
+        key: i,
+        date: `Date ${i}`,
+        hour: `${i} hr`,
+        amountPaid: ` ${i} /-`,
+    });
+}
 const BillingListIndex = () => {
+    const changeEntry = (value: any) => {
+        console.log('changed', value);
+    };
     return (
         <div>
 
@@ -26,7 +132,33 @@ const BillingListIndex = () => {
             </div>
 
             {/*    table------------------*/}
-            <BillingTable/>
+            <div>
+                <BillingTable columnsData={lastBillingColumns} dataSources={data}/>
+                <div className="my-4 w-100 flex items-center gap-8 justify-end">
+                    <Primary total={50}/>
+                    <div className="flex items-center gap-2">
+                        <button>Entry</button>
+                        <InputNumber className="w-9" controls={false} min={1} max={10} defaultValue={1} onChange={changeEntry}/>
+                    </div>
+                </div>
+            </div>
+
+            <hr className="my-10"/>
+            <div className="w-100 flex flex-row justify-between items-center gap-1 ">
+                <div className="flex flex-row items-center gap-4 ">
+                    <h2 className="mb-0 pr-3 text-heading-6 font-medium text-ct-blue-95 font-sans">
+                        All Billing Info</h2>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <button>Entry</button>
+                    <InputNumber controls={false} className="w-9" min={1} max={10} defaultValue={1} onChange={changeEntry}/>
+                </div>
+            </div>
+            <div className="mb-10">
+                <BillingTable columnsData={allBillingColumns} dataSources={anotherData}/>
+            </div>
+
         </div>
     );
 };
--- src/components/containers/billing/BillingList/BillingTable.tsx
+++ src/components/containers/billing/BillingList/BillingTable.tsx
@@ -5,75 +5,20 @@ import type {TableRowSelection} from 'antd/es/table/interface';
 import './billingTable.css'
 import Primary from "./Primary";
 import {InputNumber} from 'antd';
+import {AllBillingDataType, BillingDataType} from "./BillingListIndex";
 
-interface Person {
-    title: string;
-    subTitle: string;
+
+interface Props {
+    columnsData:ColumnsType<BillingDataType | AllBillingDataType>  ;
+    dataSources:(AllBillingDataType | BillingDataType)[];
+
+
 }
+const BillingTable = ({columnsData, dataSources}:Props) => {
 
-interface DataType {
-    key: React.Key;
-    manager: Person
-    hour: string;
-    amountPaid: string;
-}
-
-const columns: ColumnsType<DataType> = [
-    {
-        title: 'MANAGER',
-        dataIndex: 'manager',
-        width: 100,
-        render: (data) => (
-            <>
-                <p>{data.title}</p>
-                <p>{data.subTitle}</p>
-            </>
-        )
-
-    },
-    {
-        title: 'HOUR ',
-        dataIndex: 'hour',
-        width: 100,
-
-
-    },
-    {
-        title: 'AMOUNT PAID',
-        dataIndex: 'amountPaid',
-        width: 100,
-
-    },
-];
-
-const data: DataType[] = [];
-for (let i = 0; i < 7; i++) {
-    data.push({
-        key: i,
-        manager: {
-            title: `Edward King ${i}`,
-            subTitle: `sub-title ${i}`,
-
-        },
-        hour: '32',
-        amountPaid: `London, Park Lane no. ${i}`,
-    });
-}
-
-const BillingTable = () => {
-    const changeEntry = (value: any) => {
-        console.log('changed', value);
-    };
     return (
         <div className="billing-table my-2">
-            <Table columns={columns} dataSource={data} pagination={false}/>
-            <div className="my-4 w-100 flex items-center gap-8 justify-end">
-                <Primary total={100}/>
-               <div className="flex items-center gap-2">
-                   <p>Entry</p>
-                   <InputNumber min={1} max={10} defaultValue={3} onChange={changeEntry}/>
-               </div>
-            </div>
+            <Table columns={columnsData} dataSource={dataSources} pagination={false}/>
         </div>
     );
 };
