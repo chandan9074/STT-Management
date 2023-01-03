0e0437a4dc0f7d9432685e9fc237d5f190c39fb6
raihan-ops <tasnimraihan28@gmail.com>
2023-01-03 18:41:12 +0600

    feature:domain-wise

--- src/components/containers/dashboard/DataContainer/GraphTooltip.tsx
+++ src/components/containers/dashboard/DataContainer/GraphTooltip.tsx
@@ -13,17 +13,17 @@ const GraphTooltip = ({
   titleColor: string;
   align: "left" | "right" | "center";
 }) => {
-  console.log("data", data);
+  console.log("data", titleColor);
   return (
     <div className="px-5 py-6 bg-tooltip-bg rounded-[12px]">
       <h1 className={`text-base font-semibold ${titleColor} mb-0`}>
-        {data.name}
+        {data?.name}
       </h1>
       <div className="flex items-center gap-x-1 mt-4">
         <div className="py-1 px-2 rounded-[4px] bg-secondary-blue-50 bg-opacity-[0.12] w-24">
           <h3 className="mb-0 text-white text-xxs">Received</h3>
           <h3 className="mb-0 text-blue-10 font-medium">
-            {data.totalReceived}h
+            {data?.totalReceived}h
           </h3>
         </div>
         <div
@@ -31,33 +31,32 @@ const GraphTooltip = ({
         >
           <h3 className="mb-0 text-white text-xxs">Valid</h3>
           <h3 className="mb-0 text-blue-10 font-medium">
-            {data.totalReceived}h
+            {data?.totalReceived}h
           </h3>
         </div>
         <div className="py-1 px-2 rounded-[4px] bg-medium-purple-bg bg-opacity-[0.12] w-24">
           <h3 className="mb-0 text-white text-xxs">Received</h3>
           <h3 className="mb-0 text-blue-10 font-medium">
-            {data.totalReceived}h
+            {data?.totalReceived}h
           </h3>
         </div>
       </div>
       <div className="w-full border-t border-blue-gray-75 bg-opacity-50 border-dashed mt-5" />
       <div className="mt-2 flex items-center">
-        <img src={Icons.schedule} alt="schedule" />
+        <img src={Icons?.schedule} alt="schedule" />
         <h4 className="text-small text-white mb-0 ml-2">
-          Last Update: {data.lastUpdate}
+          Last Update: {data?.lastUpdate}
         </h4>
       </div>
       <img
         src={Icons.blackDropArrow}
         alt=""
-        className={`w-10 h-6 absolute -bottom-3.5 ${
-          align === "left"
-            ? "left-5"
-            : align === "right"
+        className={`w-10 h-6 absolute -bottom-3.5 ${align === "left"
+          ? "left-5"
+          : align === "right"
             ? "right-5"
             : " left-1/2 transform -translate-x-1/2"
-        }`}
+          }`}
       />
     </div>
   );
--- src/components/containers/dashboard/DataContainer/Graphs/DomainWise.tsx
+++ src/components/containers/dashboard/DataContainer/Graphs/DomainWise.tsx
@@ -1,6 +1,7 @@
-import React, {useCallback, useEffect, useState} from "react";
-import {PieChart, Pie, Cell, Tooltip} from "recharts";
-import {createCollectData} from "../../../../../data/dashboard/createCollectData";
+import React, { useCallback, useState } from "react";
+import { PieChart, Pie, Cell, Tooltip, Sector } from "recharts";
+import { createCollectData } from "../../../../../data/dashboard/createCollectData";
+import GraphTooltip from "../GraphTooltip";
 
 // const data = [
 //     {name: "Group A", value: 100},
@@ -15,55 +16,194 @@ import {createCollectData} from "../../../../../data/dashboard/createCollectData
 // ];
 
 const COLORS = ["#F5427F", "#00B86E", "#E4F542", "#42E0F5", "#3BA2F5", "#B336C8", "#F54542", "#F5AC42", "#FFD145"];
+const ActiveColor: any = {
+    "Natural & Pure Science": {
+        fillColor: "#D11263",
+        borderColor: "#F5427F",
+        textColor: "",
+        titeleColor: "text-[#F5427F]",
+        validBgColor: "bg-[#F5427F1F]"
+    },
+    "Arts": {
+        fillColor: "#00944D",
+        borderColor: "#00B86E",
+        textColor: "",
+        titeleColor: "text-[#00B86E]",
+        validBgColor: "bg-[#00B86E1F]"
+    },
+    "Belief & Thought": {
+        fillColor: "#ADC100",
+        borderColor: "#E4F542",
+        textColor: "",
+        titeleColor: "text-[#E4F542]",
+        validBgColor: "bg-[#E4F5421F]"
+    },
+    "Applied Science": {
+        fillColor: "#00B2C6",
+        borderColor: "#42E0F5",
+        textColor: "",
+        titeleColor: "text-[#42E0F5]",
+        validBgColor: "bg-[#42E0F51F]"
+    },
+    "commerce & Finance": {
+        fillColor: "#0081D0",
+        borderColor: "#3BA2F5",
+        textColor: "",
+        titeleColor: "text-[#3BA2F5]",
+        validBgColor: "bg-[#3BA2F51F]"
+    },
+    "Leisure": {
+        fillColor: "#88009F",
+        borderColor: "#B336C8",
+        textColor: "",
+        titeleColor: "text-[#B336C8]",
+        validBgColor: "bg-[#B336C8]"
+    },
+    "Literature": {
+        fillColor: "#D11C29",
+        borderColor: "#F54542",
+        textColor: "",
+        titeleColor: "text-[#F54542]",
+        validBgColor: "bg-[#F545421F]"
+    },
+    "World & Current Affairs": {
+        fillColor: "#C78415",
+        borderColor: "#F5AC42",
+        textColor: "",
+        titeleColor: "text-[#F5AC42]",
+        validBgColor: "bg-[#F5AC421F]"
+
+    },
+    "Social & Community": {
+        fillColor: "#CBA302",
+        borderColor: "#FFD145",
+        textColor: "",
+        titeleColor: "text-[#FFD145]",
+        validBgColor: "bg-[#FFD1451F]"
+    },
+}
 
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({
-                                   cx,
-                                   cy,
-                                   midAngle,
-                                   innerRadius,
-                                   outerRadius,
-                                   percent,
-                                   index,
-                                   data
-                               }: any) => {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+    payload
+
+}: any) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.3;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
+    let _percent: string = (percent * 100).toFixed(0)
     return (
         <text
             x={x}
             y={y}
-            fill="white"
+            fill={payload.fill === "#E4F542" ? "black" : "white"}
             textAnchor={x > cx ? "start" : "end"}
             dominantBaseline="central"
         >
-            {`${(percent * 100).toFixed(0)}%`}
+            {
+                parseInt(_percent) >= 15 ? `${_percent}%` : ''
+            }
         </text>
     );
 };
 
-const CustomTooltip = () => {
+const renderActiveShape = (props: any) => {
+    const RADIAN = Math.PI / 180;
+    const {
+        cx,
+        cy,
+        innerRadius,
+        outerRadius,
+        startAngle,
+        endAngle,
+        midAngle,
+        payload
+    } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius - 180) * cos;
+    const sy = cy + (outerRadius - 180) * sin;
     return (
-        <div>
-            <p>Hello</p>
+        <Sector
+            cx={sx}
+            cy={sy}
+            innerRadius={innerRadius - 2}
+            outerRadius={outerRadius + 2}
+            startAngle={startAngle}
+            endAngle={endAngle}
+            fill={ActiveColor[payload.name]?.fillColor}
+            stroke={ActiveColor[payload.name]?.borderColor}
+
+
+        />
+    );
+};
+const CustomTooltip = ({ payload }: any) => {
+    const listData: any = createCollectData
+    // console.log("hello", listData.data[0].domainWise.filter((item:any) => item.name ===payload[0]?.payload?.name) [0]);
+    console.log('---------------', payload[0]?.payload)
+    return (
+        <div className="flex justify-start">
+            <GraphTooltip
+                align="center"
+                data={listData.data[0].domainWise.filter((item: any) => item.name === payload[0]?.payload?.name)[0]}
+                validBgColor={ActiveColor[payload[0]?.payload?.name]?.validBgColor}
+                titleColor={ActiveColor[payload[0]?.payload?.name]?.titeleColor}
+            />
         </div>
     );
 };
 const DomainWise = () => {
+    const [activeIndex, setActiveIndex] = useState<any>(0);
+    const [activeData, setActiveData] = useState<any>()
+    const [top, setTop] = useState<number>(0);
+    const [left, setLeft] = useState<number>(0)
+    // const onMouseOver = useCallback((data: any, index: any, event: any) => {
+    //     // if(index !== activeIndex){
+    //     //     setActiveIndex(index);
+    //     //     setTop(event.clientY)
+    //     //     setLeft(event.clientX)
+    //     //     setActiveData(data)
+    //     // }
+    //     setTop(event.clientY)
+    //     setLeft(event.clientX)
+    //     console.log("hello vasdi f adf", index, activeIndex)
+    // }, []);
+    const onMouseOver = (data: any, index: any, event: any) => {
+        setActiveIndex(index);
+        setTop(event.clientY)
+        setLeft(event.clientX)
+        setActiveData(data)
+        console.log("asdkjfhak")
+    }
+    const onMouseLeave = useCallback((data: any, index: any) => {
+        setActiveIndex(null);
+    }, []);
     const listData: any = createCollectData
     let domainData: any = []
-    const res = listData.data[0].domainWise.map((e: any) =>
+    listData.data[0].domainWise.map((e: any) =>
         domainData.push({
             name: e.name,
             value: e.contribution
         })
     )
+
+    // console.log("data", activeData)
+
     return (
-        <div >
+        <div>
             <PieChart width={400} height={400}>
                 <Pie
+                    isAnimationActive={false}
+                    animationDuration={0}
                     data={domainData}
                     cx={200}
                     cy={200}
@@ -76,17 +216,38 @@ const DomainWise = () => {
                     dataKey="value"
                     startAngle={90}
                     endAngle={-360}
+                    activeIndex={activeIndex}
+                    onMouseOver={onMouseOver}
+                    onMouseLeave={onMouseLeave}
+                    activeShape={renderActiveShape}
+
                 >
                     {COLORS.map((entry: any, index: any) => (
+
                         <Cell
                             key={`cell-${index}`}
                             fill={COLORS[index % COLORS.length]}
+                            style={{ position: "relative" }}
+                        >
+                        </Cell>
 
-                        />
                     ))}
                 </Pie>
-                <Tooltip content={<CustomTooltip/>}/>
+                {/* <Tooltip
+                    // wrapperStyle={{ top: tooltipPosition[activeIndex]?.top, left: tooltipPosition[activeIndex]?.left, display:"flex", justifyContent:"flex-start" }}
+                    content={<CustomTooltip />} /> */}
             </PieChart>
+            {activeIndex === 0 || activeIndex === 1 || activeIndex === 2 || activeIndex === 3 || activeIndex === 4 || activeIndex === 5 || activeIndex === 6 || activeIndex === 7 || activeIndex === 8 ? <div style={{ position: "absolute", top: top + 180, left: left - 50 }}>
+
+                {/* <h1 className="text-white">{activeData?.payload?.name}</h1> */}
+                <GraphTooltip
+                    align="left"
+                    data={listData.data[0].domainWise.filter((item: any) => item.name === activeData?.payload?.name)[0]}
+                    validBgColor={ActiveColor[activeData?.payload?.name]?.validBgColor}
+                    titleColor={ActiveColor[activeData?.payload?.name]?.titeleColor}
+                />
+            </div> : ""}
+
         </div>
     );
 };
