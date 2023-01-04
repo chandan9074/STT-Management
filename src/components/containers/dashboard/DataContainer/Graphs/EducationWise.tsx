62f4cb8110a9e1bba35be1dd0f63cbe3e326c380
jalal.seu.cse <jalal.seu.cse@gmail.com>
2023-01-04 10:30:23 +0600

    education wise complete

--- src/components/containers/dashboard/DataContainer/Graphs/EducationWise.tsx
+++ src/components/containers/dashboard/DataContainer/Graphs/EducationWise.tsx
@@ -28,14 +28,14 @@ const EducationWise = ({ data }: { data: createCollectSimilarPropertyDT[] }) => 
 
 
     return (
-        <div className=' pt-10  pb-5 relative h-[400px] mt-2'>
+        <div className=' pt-10  pb-5 relative h-[400px]'>
             <div>
                 {
                     percentData?.map((value, index) => (
                         <div
                             key={index}
                             style={{
-                                marginBottom: dimensionValue.length - 1 === index ? dimensionValue[index] : dimensionValue[index] - dimensionValue[index + 1] -20,
+                                marginBottom: dimensionValue.length - 1 === index ? dimensionValue[index] : dimensionValue[index] - dimensionValue[index + 1] - 20,
 
                             }}
 
@@ -43,7 +43,6 @@ const EducationWise = ({ data }: { data: createCollectSimilarPropertyDT[] }) => 
                         >
                             <div>
                                 {
-                                    // <h1>{dimensionValue.length -1 === index ? dimensionValue[index] : (dimensionValue[index] - dimensionValue[index+1])}</h1>
                                     value !== 100 &&
                                     <h1 className='text-ct-blue-45 text-[14px]'>{value}%</h1>
 
@@ -62,49 +61,47 @@ const EducationWise = ({ data }: { data: createCollectSimilarPropertyDT[] }) => 
                     {
                         educationWiseData?.map((value: any, index) => (
                             <div key={index} className='flex flex-col justify-center items-center'>
-                                <div className='rounded-[4px] text-white flex justify-center w-[50px] bg-opacity-[85%] bg-blue-gray-85 h-8 flex justify-center items-center'>
+                                <div className='rounded-[4px] text-white bg-opacity-[85%] bg-blue-gray-85  items-center py-1 px-[6px] mb-[3px]'>
                                     <h1 className='text-[12px]'>{Math.round((100 * educationWiseDataHeights[index]) / maxValue)}%</h1>
                                 </div>
                                 <div className='relative group'>
                                     <div
                                         style={{
                                             height: `${educationWiseDataHeights[index]}px`,
-                                            backgroundColor: `${index % 3 === 0
-                                                ? "#DAD7FE"
-                                                : index % 4 === 0
-                                                    ? "#FFD3D3"
-                                                    : index % 5 === 0
-                                                        ? "#DAD7FE"
-                                                        : index % 2 === 0
-                                                            ? "#CCF8FE"
-                                                            : "#FFD3D3"
-                                                }`,
                                         }}
-                                        className={`w-[84px]`} id="triangle"
+                                        className={`w-[84px] ${index % 3 === 0
+                                            ? "bg-purple-A10 hover:bg-cobalite-90"
+                                            : index % 4 === 0
+                                                ? "bg-red-15 hover:bg-coral-90"
+                                                : index % 5 === 0
+                                                    ? "bg-purple-A10 hover:bg-cobalite-90"
+                                                    : index % 2 === 0
+                                                        ? "bg-green-A10 hover:bg-[#94D676]"
+                                                        : "bg-red-15 hover:bg-coral-90"
+                                            } duration-300`} id="triangle"
                                     >
                                     </div>
-                                    <div className={`z-50 animate-fadeIn absolute top-[-203px] hidden group-hover:block`} >
-                                            <GraphTooltip
-                                                data={value}
-                                                validBgColor="bg-coral-90"
-                                                titleColor={`${index % 3 === 0
-                                                    ? "text-[#DAD7FE]"
-                                                    : index % 4 === 0
-                                                        ? "text-[#FFD3D3]"
-                                                        : index % 5 === 0
-                                                            ? "text-[#DAD7FE]"
-                                                            : index % 2 === 0
-                                                                ? "text-[#CCF8FE]"
-                                                                : "text-[#FFD3D3]"
-                                                    }`}
-                                                align="left"
-                                            />
-                                        </div>
+                                    <div className={`z-50 animate-fadeIn absolute top-[-208px] hidden group-hover:block`} >
+                                        <GraphTooltip
+                                            data={value}
+                                            validBgColor="bg-coral-90"
+                                            titleColor={`${index % 3 === 0
+                                                ? "text-purple-A10"
+                                                : index % 4 === 0
+                                                    ? "text-red-15"
+                                                    : index % 5 === 0
+                                                        ? "text-purple-A10"
+                                                        : index % 2 === 0
+                                                            ? "text-green-A10"
+                                                            : "text-red-15"
+                                                }`}
+                                            align="left"
+                                        />
+                                    </div>
                                 </div>
 
-                                <div className={`flex justify-center absolute items-start ${value?.name === "Graduate & Postgraduate" ? "-bottom-11":"-bottom-6" } w-32`}>
+                                <div className={`flex justify-center absolute items-start ${value?.name === "Graduate & Postgraduate" ? "-bottom-11" : "-bottom-6"} w-32`}>
                                     <h1 className='text-ct-blue-90 text-[13px] font-semibold text-center'>
-                                        {/* primary */}
                                         {value?.name}
                                     </h1>
                                 </div>
