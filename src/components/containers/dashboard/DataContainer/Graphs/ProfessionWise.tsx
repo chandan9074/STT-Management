71675a9772970140fd29e3d5de2e74606c9585d2
raihan-ops <tasnimraihan28@gmail.com>
2023-01-08 11:27:06 +0600

    fix:font-chnages

---
+++ src/components/containers/dashboard/DataContainer/Graphs/ProfessionWise.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfessionWise = () => {
+    return (
+        <div>
+            <h1 style={{fontFamily:"Fira Sans"}}>buggy</h1>
+        </div>
+    );
+};
+
+export default ProfessionWise;
