cfd00a149ec01ff859fa8ce00630341aa521b623
jalal.seu.cse <jalal.seu.cse@gmail.com>
2023-01-15 17:09:03 +0600

    recording distance almost done

--- src/components/common/Circle/Circle1.tsx
+++ src/components/common/Circle/Circle1.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createCollectSimilarPropertyDT } from '../../../types/dashboardTypes';
+import GraphTooltip from '../../containers/dashboard/DataContainer/GraphTooltip';
 
 interface Props {
     bgColor: string | undefined;
@@ -9,15 +10,21 @@ interface Props {
     value: number;
     height: string;
     width: string;
-    // maxValue: number;
-    // barHeight: number;
+    indexNumber: number;
+    data: createCollectSimilarPropertyDT,
+    tooltipTitleColor: string
+
 }
 
-const Circle1 = ({ bgColor, ringColor, textColor, value, shadowColor, height, width }: Props) => {
+const Circle1 = ({ bgColor, ringColor, textColor, value, shadowColor, height, width, indexNumber, data, tooltipTitleColor }: Props) => {
 
     // console.log('barhe', barHeight);
     // console.log('value', value);
     // console.log('maxValue', maxValue);
+
+    console.log('data', data);
+
+
 
 
 
@@ -26,17 +33,38 @@ const Circle1 = ({ bgColor, ringColor, textColor, value, shadowColor, height, wi
 
         >
             <div
-                className={`${height} ${width} z-20 text-sm font-medium  ${bgColor} rounded-full flex justify-center items-center ring-2 ${ringColor} hover:ring-white hover:ring-0  ${shadowColor} hover:ring-offset-2 transition duration-4000 ease-out hover:ease-in duration-300`}
+                className={`${height} ${width} z-20 text-sm font-medium  ${bgColor} rounded-full flex justify-center items-center ring-2 ${ringColor} hover:ring-white hover:ring-0  ${shadowColor} hover:ring-offset-2 transition duration-4000 ease-out hover:ease-in duration-300 group`}
             >
+                <div className={`z-50 animate-fadeIn absolute top-[-208px] hidden group-hover:block`} >
+                    <GraphTooltip
+                        data={data && data}
+                        validBgColor="bg-coral-90"
+                        titleColor={tooltipTitleColor}
+                        align="left"
+                    />
+                </div>
                 {
+                    indexNumber <= 2 &&
 
-                    <h1 className={`${textColor} text-[13px]`}>
+                    <h1 className={`${textColor} text-[13px] group-hover:hidden`}>
+
                         {value}%
                     </h1>
 
                 }
+
+
+                {
+                    value > 7 &&
+                    <h1 className={`${textColor} text-[13px] hidden group-hover:block`}>
+
+                        {value}%
+                    </h1>
+                }
+
+
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/components/containers/dashboard/DataContainer/Graphs/RecordingDistance/Graph.tsx
+++ src/components/containers/dashboard/DataContainer/Graphs/RecordingDistance/Graph.tsx
@@ -1,70 +1,41 @@
-import { abort } from 'process';
-import React, { useEffect, useState } from 'react';
-import { getValueFromPercentages } from '../../../../../../helpers/Utils';
 import { createCollectSimilarPropertyDT } from '../../../../../../types/dashboardTypes';
 import Circle1 from '../../../../../common/Circle/Circle1';
-
-const value = [
-    {
-        contribution: 33
-    },
-    {
-        contribution: 18
-    },
-    {
-        contribution: 12
-
-    },
-    {
-        contribution: 13
-    },
-    {
-        contribution: 13
-    },
-    {
-        contribution: 13
-    },
-    {
-        contribution: 13
-    },
-]
-
 
 const color = [
     {
         bgColor: 'bg-red-15',
         ringColor: 'ring-[#E8C8C8]',
-        shadowColor: 'hover:shadow-light-tomato'
+        shadowColor: 'hover:shadow-light-tomato',
     },
     {
         bgColor: 'bg-green-A10',
         ringColor: 'ring-[#D1E8C7]',
-        shadowColor: 'hover:shadow-green-A10'
+        shadowColor: 'hover:shadow-green-A10',
     },
     {
         bgColor: 'bg-[#FFF5CC]',
         ringColor: 'ring-[#E8DFBA]',
-        shadowColor: 'hover:shadow-green-A10'
+        shadowColor: 'hover:shadow-green-A10',
     },
     {
         bgColor: 'bg-[#FFE5D3]',
         ringColor: 'ring-[#E8D0C0]',
-        shadowColor: 'hover:shadow-light-orange-shadow'
+        shadowColor: 'hover:shadow-light-orange-shadow',
     },
     {
         bgColor: 'bg-[#CCF8FE]',
         ringColor: 'ring-[#BAE3E8]',
-        shadowColor: 'hover:shadow-green-A10'
+        shadowColor: 'hover:shadow-green-A10',
     },
     {
         bgColor: 'bg-[#CCDDFE]',
         ringColor: 'ring-[#BACAE8]',
-        shadowColor: 'hover:shadow-green-A10'
+        shadowColor: 'hover:shadow-green-A10',
     },
     {
         bgColor: 'bg-[#DAD7FE]',
         ringColor: 'ring-[#CAC8E8]',
-        shadowColor: 'hover:shadow-green-A10'
+        shadowColor: 'hover:shadow-green-A10',
     }
 
 
@@ -73,154 +44,45 @@ const color = [
 
 const Graph = ({ data }: { data: createCollectSimilarPropertyDT[] }) => {
 
-    // const [barHeight, setBarHeight] = useState<number>(452);
-    const height = 158;
-    const [barHeight, setBarHeight] = useState<number>(height);
-    const [heighPercentage, setheightPercentage] = useState(0);
-    const [dimesion, setDimension] = useState<number[]>([]);
-
-    const [circlePosition, setCirclePosition] = useState({
-        circleOneBottom: 154,
-        circleOneLeft: 113,
-        // circleTwoTop: 25,
-        // cicleTwoLeft: 164,
-        circleTwoTop: 8,
-        cicleTwoLeft: 163,
-        circleThreeBottom: 167,
-        circleThreeRight: 59,
-        circleFourBottom: 78,
-        circleFoureft: 46,
-
-    })
-
-    useEffect(() => {
-
-        const valueOnePercentage = (value[1].contribution * 100) / value[0].contribution;
-        setheightPercentage(valueOnePercentage)
-        console.log('%%%%%%%%%%%-----------', value[2].contribution - value[4].contribution);
-        console.log('-----------', value[2].contribution - value[4].contribution <= 3 ? 88 : 52);
-
-
-        // if ((value[0].contribution - value[1].contribution) <= 10) {
-        if (valueOnePercentage >= 83) {
-
-            // setBarHeight(83)
-            setBarHeight(95)
-
-        }
-        // else if ((value[0].contribution - value[1].contribution) <= 20) {
-        else if (valueOnePercentage >= 66) {
-            setBarHeight(110)
-            // setBarHeight(66)
-        }
-        else if (valueOnePercentage >= 50) {
-            setBarHeight(110)
-
-        }
-
-        else {
-            setBarHeight(158);
-        }
-    }, [value]);
-
-    useEffect(() => {
-        getPosition();
-    }, [barHeight]);
-
-    const getPosition = () => {
-        // let _cirlcleBottom;
-        if (barHeight === 95) {
-            const _cirlcleBottom = ((160 * barHeight) / height) - 7;
-            const _cirlcleOneLeft = ((160 * barHeight) / height - 32);
-
-            const _circleTwoLeft = ((166 * barHeight) / height - 24 + 30);
-            const _circleTwoTop = ((2 * barHeight) / height - 24 + 36);
-
-            const _circleThreeBottom = ((166 * barHeight) / height - 24 + 30);
-            const _circleThreeRight = ((59 * barHeight) / height - 24 + 30);
-
-            // const _circleFourBottom = (barHeight * value[2].contribution) / value[0].contribution +3 ;
-            // const _circleFourLeft = ((59 * barHeight) / height - 24 + 30)  + 20;
-
-            const _circleFourBottom = (barHeight * value[2].contribution) / value[0].contribution + 3;
-            const _circleFourLeft = (barHeight * value[2].contribution) / value[0].contribution - 20;
-
-            //     circleTwoTop: 2,
-            // cicleTwoLeft: 166,
-            setCirclePosition({
-                ...circlePosition,
-                circleOneBottom: _cirlcleBottom ? _cirlcleBottom : circlePosition.circleOneBottom,
-                circleOneLeft: _cirlcleOneLeft ? _cirlcleOneLeft : circlePosition.circleOneLeft,
-                cicleTwoLeft: _circleTwoLeft,
-                circleTwoTop: _circleTwoTop,
-                circleThreeBottom: _circleThreeBottom,
-                circleThreeRight: _circleThreeRight,
-                circleFourBottom: _circleFourBottom,
-                circleFoureft: _circleFourLeft
-            });
-
-        } else if (barHeight === 110) {
-
-            const _cirlcleBottom = ((160 * barHeight) / height - 10);
-            const _cirlcleOneLeft = ((160 * barHeight) / height - 20);
-
-            const _circleTwoLeft = ((166 * barHeight) / height - 24 + 30);
-            const _circleTwoTop = ((2 * barHeight) / height - 24 + 40);
-
-            const _circleThreeBottom = ((166 * barHeight) / height - 24 + 30);
-            const _circleThreeRight = ((59 * barHeight) / height - 24 + 30 - 15);
-
-            const _circleFourBottom = (barHeight * value[2].contribution) / value[0].contribution;
-            const _circleFourLeft = ((59 * barHeight) / height - 24 + 30) + 20;
-
-            setCirclePosition({
-                ...circlePosition,
-                circleOneBottom: _cirlcleBottom ? _cirlcleBottom : circlePosition.circleOneBottom,
-                circleOneLeft: _cirlcleOneLeft ? _cirlcleOneLeft : circlePosition.circleOneLeft,
-                cicleTwoLeft: _circleTwoLeft ? _circleTwoLeft : circlePosition.cicleTwoLeft,
-                circleTwoTop: _circleTwoTop,
-                circleThreeBottom: _circleThreeBottom,
-                circleThreeRight: _circleThreeRight,
-                circleFourBottom: _circleFourBottom,
-                circleFoureft: _circleFourLeft
-            });
-        } else {
-
-            setCirclePosition({
-                ...circlePosition,
-                circleOneBottom: 154,
-                circleOneLeft: 113,
-                // circleTwoTop: 25,
-                // cicleTwoLeft: 164,
-                circleTwoTop: 8,
-                cicleTwoLeft: 163,
-                circleThreeBottom: 167,
-                circleThreeRight: 59,
-                circleFourBottom: (barHeight * value[2].contribution) / value[0].contribution,
-                circleFoureft: (barHeight * value[2].contribution) / value[0].contribution - 21,
-            });
-        }
-
-    }
-
-    // console.log('circleposition888888888', barHeight, circlePosition.circleOneLeft);
-
-
-
-
-
     return (
         <div className='relative'>
             <div className='absolute -top-[27px]'>
+                {/* {
+                    value?.map((m, i) => (
+                        <div
+                            className={`${i === 0 ? 'bottom-0' :
+                                i === 1 ? 'bottom-10' :
+                                    ''
+                                } absolute`}
+                        >
+                            <Circle1
+                                bgColor={color[i].bgColor}
+                                ringColor={color[i].ringColor}
+                                textColor='text-[#453D38]'
+                                shadowColor={color[3].shadowColor}
+                                value={value[i].contribution}
+                                height={i === 0 ? 'w-[157px]' : i === 1 ? 'w-[106px]' : i === 2 ? 'w-[102px]' : i === 3 ? 'w-[60px]' : i === 4 ? 'w-[53px]' : i === 5 ? 'w-[30px]' : i === 6 ? '' : ''}
+                                width={i === 0 ? 'h-[157px]' : i === 1 ? 'h-[106px]' : i === 2 ? 'h-[102px]' : i === 3 ? 'h-[60px]' : i === 4 ? 'h-[53px]' : i === 5 ? 'h-[30px]' : i === 6 ? 'w-[21px]' : 'h-[21px]'}
+                                indexNumber={i}
+
+                            // data={m}
+
+                            />
+                        </div>
+                    ))
+                } */}
                 <div className='relative'>
                     <Circle1
                         bgColor={color[0].bgColor}
                         ringColor={color[0].ringColor}
                         textColor='text-[#453D38]'
                         shadowColor={color[0].shadowColor}
-                        value={value[0].contribution}
+                        value={data[0].contribution}
                         height="w-[157px]"
                         width='h-[157px]'
+                        indexNumber={0}
+                        data={data[0]}
+                        tooltipTitleColor='text-red-15'
                     />
 
                     <div className='absolute -right-[60px] -top-[100px]'>
@@ -229,9 +91,12 @@ const Graph = ({ data }: { data: createCollectSimilarPropertyDT[] }) => {
                             ringColor={color[1].ringColor}
                             textColor='text-[#453D38]'
                             shadowColor={color[1].shadowColor}
-                            value={value[1].contribution}
+                            value={data[1].contribution}
                             height="w-[106px]"
                             width='h-[106px]'
+                            indexNumber={1}
+                            data={data[1]}
+                            tooltipTitleColor='text-green-A10'
                         />
 
                     </div>
@@ -243,9 +108,12 @@ const Graph = ({ data }: { data: createCollectSimilarPropertyDT[] }) => {
                                 ringColor={color[2].ringColor}
                                 textColor='text-[#453D38]'
                                 shadowColor={color[2].shadowColor}
-                                value={value[2].contribution}
+                                value={data[2].contribution}
                                 height="w-[102px]"
                                 width='h-[102px]'
+                                indexNumber={2}
+                                data={data[2]}
+                                tooltipTitleColor='text-yellow-A10'
                             />
 
                             <div className='absolute -top-[55px] -right-[12px]'>
@@ -255,9 +123,12 @@ const Graph = ({ data }: { data: createCollectSimilarPropertyDT[] }) => {
                                         ringColor={color[4].ringColor}
                                         textColor='text-[#453D38]'
                                         shadowColor={color[4].shadowColor}
-                                        value={value[4].contribution}
+                                        value={data[4].contribution}
                                         height="w-[53px]"
                                         width='h-[53px]'
+                                        indexNumber={4}
+                                        data={data[4]}
+                                        tooltipTitleColor='text-blue-A10'
                                     />
 
                                     <div className='absolute -top-[42px] left-[4px]'>
@@ -267,9 +138,12 @@ const Graph = ({ data }: { data: createCollectSimilarPropertyDT[] }) => {
                                                 ringColor={color[5].ringColor}
                                                 textColor='text-[#453D38]'
                                                 shadowColor={color[5].shadowColor}
-                                                value={value[5].contribution}
+                                                value={data[5].contribution}
                                                 height="w-[30px]"
                                                 width='h-[30px]'
+                                                indexNumber={5}
+                                                data={data[5]}
+                                                tooltipTitleColor='text-blue-20'
                                             />
 
                                             <div className='absolute top-[14px] -right-[34px]'>
@@ -278,9 +152,12 @@ const Graph = ({ data }: { data: createCollectSimilarPropertyDT[] }) => {
                                                     ringColor={color[6].ringColor}
                                                     textColor='text-[#453D38]'
                                                     shadowColor={color[6].shadowColor}
-                                                    value={value[6].contribution}
+                                                    value={data[6].contribution}
                                                     height="w-[21px]"
                                                     width='h-[21px]'
+                                                    indexNumber={6}
+                                                    data={data[6]}
+                                                    tooltipTitleColor='text-purple-A10'
                                                 />
                                             </div>
                                         </div>
@@ -298,16 +175,17 @@ const Graph = ({ data }: { data: createCollectSimilarPropertyDT[] }) => {
                             ringColor={color[3].ringColor}
                             textColor='text-[#453D38]'
                             shadowColor={color[3].shadowColor}
-                            value={value[3].contribution}
+                            value={data[3].contribution}
                             height="w-[60px]"
                             width='h-[60px]'
+                            indexNumber={3}
+                            data={data[3]}
+                            tooltipTitleColor='text-blue-A10'
                         />
                     </div>
-
-
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
