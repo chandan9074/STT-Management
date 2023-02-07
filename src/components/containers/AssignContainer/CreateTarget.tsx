e8c4ed4606dc39d9ee2b51ab66b49c8cdcbad85f
raihan-ops <tasnimraihan28@gmail.com>
2023-02-07 15:39:26 +0600

    merge

--- src/components/containers/AssignContainer/CreateTarget.tsx
+++ src/components/containers/AssignContainer/CreateTarget.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TargetForm from './AssssigForm/TargetForm';
+import TargetTable from './TargetTable';
 
 const CreateTarget = () => {
     return (
---
+++ src/components/containers/AssignContainer/TargetTable/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TargetTable = () => {
+    return (
+        <div className='mt-10 mx-5'>
+            <p>1000 Targets</p>
+
+        </div>
+    );
+};
+
+export default TargetTable;
