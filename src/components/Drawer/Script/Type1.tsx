22883fc2bb002d946f21309e7d23b382596830d2
chandan1001 <chandan15-11611@diu.edu.bd>
2023-02-07 17:59:30 +0600

    ui: create working on add script

--- src/components/Drawer/Script/Type1.tsx
+++ src/components/Drawer/Script/Type1.tsx
@@ -1,7 +1,11 @@
 import { Drawer } from 'antd';
 import React from 'react';
 import Icons from '../../../assets/Icons';
+import { filterData } from '../../../data/script/filter';
 import Buttons from '../../Buttons';
+import { Filter } from '../../Filter';
+import { SearchBox } from '../../SearchBox';
+import Table from '../../Table';
 
 type Props = {
     isDrawerOpen: boolean,
@@ -24,17 +28,24 @@ const Type1 = ({ isDrawerOpen, drawerClose, title }: Props) => {
             width='555px'
         >
             <div className='bg-ct-blue-05'>
-                <div className=' flex items-center gap-x-[28px] py-[24px] px-[23px]'>
-                    <Buttons.IconButton.Circle
-                        size='medium'
-                        variant="CT-Blue"
-                        icon={<img src={Icons.CloseIconButton} alt="" />}
-                        border='border'
-                        background="white"
-                        onClick={() => onClose()}
-                    />
-                    <h1 className='text-ct-blue-95 text-[18px] font-medium'>{title}</h1>
+                <div className='p-6'>
+                    <div className='flex items-center gap-x-7 mb-6'>
+                        <Buttons.IconButton.Circle
+                            size='medium'
+                            variant="CT-Blue"
+                            icon={<img src={Icons.CloseIconButton} alt="" />}
+                            border='border'
+                            background="white"
+                            onClick={() => onClose()}
+                        />
+                        <h1 className='text-ct-blue-95 text-[18px] font-medium'>{title}</h1>
+                    </div>
+                    <div className='inline-flex items-center'>
+                        <SearchBox.Type1 inputWidth='w-[172px]' placeholder='Search' bgColor='bg-blue-gray-A10' paddingX='px-3' paddingY='py-2' textColor='text-blue-gray-80' />
+                        <Filter.Type1 filterData={filterData} align="center" />
+                    </div>
                 </div>
+                <Table.Type9 />
             </div>
 
         </Drawer>
--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -5,7 +5,7 @@ import Type4 from "./Type4";
 import Type5 from "./Type5";
 import Type6 from "./Type6";
 import Type7 from "./Type7";
-
-const Table = { Type1, Type2, Type3, Type4, Type5, Type6, Type7 };
+import Type9 from "./Type9";
+const Table = { Type1, Type2, Type3, Type4, Type5, Type6, Type7, Type9 };
 
 export default Table;
