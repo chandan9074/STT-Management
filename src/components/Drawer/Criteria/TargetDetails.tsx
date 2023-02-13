1189dc1b6289faa18dce24f8e81d8bee88fd6952
jalal.seu.cse <jalal.seu.cse@gmail.com>
2023-02-13 19:05:51 +0600

    create criteria almost done

--- src/components/Drawer/Criteria/TargetDetails.tsx
+++ src/components/Drawer/Criteria/TargetDetails.tsx
@@ -124,7 +124,7 @@ const TargetDetails = ({ onLengthClickClose }: { onLengthClickClose: () => void 
             </div>
             {/* ----------------------------------------------------------- */}
 
-            <div className='mt-[25px] flex'>
+            <div className='mt-3 flex'>
                 <div>
                     {singleValue1?.map((item: any, i: number) => (
                         <div className={` grid grid-cols-12`} key={i}>
@@ -138,8 +138,8 @@ const TargetDetails = ({ onLengthClickClose }: { onLengthClickClose: () => void 
                             <div className="col-span-8 pt-3 pr-2 pl-3">
                                 {item?.title === "Division/ District" ? (
                                     <h1 className="text-blue-gray-80 font-medium text-14px  leading-15px">
-                                        {item?.value?.map((value: string) => {
-                                            return value + ", ";
+                                        {item?.value?.map((value: string, j: number) => {
+                                            return value + `${(item.value.length-1) !== j ? ', ' : ''}`;
                                         })}
                                     </h1>
                                 ) : (
@@ -180,8 +180,8 @@ const TargetDetails = ({ onLengthClickClose }: { onLengthClickClose: () => void 
                                     <h1 className="text-blue-gray-80 font-medium text-14px  leading-15px">
                                         {
                                             item.value?.length > 0 ?
-                                                item?.value?.map((value: string) => {
-                                                    return value + ', ';
+                                                item?.value?.map((value: string, j:number) => {
+                                                    return value + `${(item.value.length-1) !== j ? ', ' : ''}`;
                                                 }) :
                                                 '-'
                                         }
