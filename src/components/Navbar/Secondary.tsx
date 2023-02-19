2da4653d9d4d61c374bd2c959a5213c23eded603
jalal.seu.cse <jalal.seu.cse@gmail.com>
2023-02-19 18:48:45 +0600

    scondary navbar breadcamb problem almost solved for assign

--- src/components/Navbar/Secondary.tsx
+++ src/components/Navbar/Secondary.tsx
@@ -1,33 +1,45 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import Icons from "../../assets/Icons";
-import { CREATE_TARGET_PATH } from '../../helpers/Slug';
+import { Link, useLocation } from 'react-router-dom';
+import Icons from '../../assets/Icons';
+import { ROOT_PATH } from '../../helpers/Slug';
 import { slugFormatter } from '../../helpers/Utils';
 
 const Secondary = () => {
-
   const location = useLocation();
 
+  const paths = location.pathname.split('/').slice(1);
+  const breadcrumbs = paths.map((path, index) => ({
+    name: slugFormatter(path),
+    path: `${ROOT_PATH}${paths.slice(0, path==="assign"? index+2:index+1).join('/')}`,
+  }));
+
   return (
-    <div className="flex justify-between items-center pt-4 pb-2 pr-4 pl-[73px] fixed w-full z-[90] bg-white">
-      <div className={`${location.pathname === CREATE_TARGET_PATH ? 'ml-[23px]' : 'ml-[240px]'} flex items-center`}>
-        {
-          location.pathname.split('/').slice(1)?.map((value, i) => (
-            <div key={i} className='flex items-center gap-x-[4px]'>
-              <h1 className={`${location.pathname.split('/').length - 2 === i ? 'text-ct-blue-60 font-medium' : 'text-blue-gray-A50 '} text-xxs`}>{slugFormatter(value)}</h1>
-              <div className={`${location.pathname.split('/').length - 2 === i ? 'hidden' : ''} w-[18px] h-[18px] flex justify-center items-center`}>
-                <img className='w-[5px] h-[8px]' src={Icons.ChevronRight} alt="" />
+    <div className="flex justify-between items-center pt-4 pb-2 pr-4 pl-[97px] fixed w-full z-[90] bg-white">
+      <div className="flex items-center">
+        {breadcrumbs.map((breadcrumb, index) => (
+          <React.Fragment key={breadcrumb.path}>
+            <Link to={breadcrumb.path}>
+              <h1 className={`${
+                  index === breadcrumbs.length - 1 
+                  ? 'text-ct-blue-60 font-medium' 
+                  : 'text-blue-gray-A50 '
+                } text-xxs`}
+              >
+                {breadcrumb.name}
+              </h1>
+            </Link>
+            {index < breadcrumbs.length - 1 && (
+              <div className="w-[18px] h-[18px] flex justify-center items-center">
+                <img className="w-[5px] h-[8px]" src={Icons.ChevronRight} alt="" />
               </div>
-            </div>
-          ))
-        }
+            )}
+          </React.Fragment>
+        ))}
       </div>
       <div className="flex items-center">
         <img src={Icons.admin} alt="user1" className="w-6 h-6 mr-2" />
         <div>
-          <h4 className="text-xxs text-ct-blue-95 font-medium mb-0.5">
-            Md. Jalal Uddin
-          </h4>
+          <h4 className="text-xxs text-ct-blue-95 font-medium mb-0.5">Md. Jalal Uddin</h4>
           <h5 className="text-xxs text-blue-gray-75 mb-0">Admin</h5>
         </div>
         <button className="ml-5">
@@ -39,3 +51,55 @@ const Secondary = () => {
 };
 
 export default Secondary;
+
+
+
+
+
+// import React from 'react';
+// import { Link, useLocation } from 'react-router-dom';
+// import Icons from "../../assets/Icons";
+// import { CREATE_TARGET_PATH, ROOT_PATH } from '../../helpers/Slug';
+// import { slugFormatter } from '../../helpers/Utils';
+
+// const Secondary = () => {
+
+//   const location = useLocation();
+
+//   console.log('location--------', location.pathname);
+  
+
+  
+
+//   return (
+//     <div className="flex justify-between items-center pt-4 pb-2 pr-4 pl-[73px] fixed w-full z-[90] bg-white">
+//       <div className={`${location.pathname === CREATE_TARGET_PATH ? 'ml-[23px]' : 'ml-[240px]'} flex items-center`}>
+//         {
+//           location.pathname.split('/').slice(1)?.map((value, i) => (
+//           <Link to={`${ROOT_PATH}${value}`} key={i}>
+//             <div className='flex items-center gap-x-[4px]'>
+//               <h1 className={`${location.pathname.split('/').length - 2 === i ? 'text-ct-blue-60 font-medium' : 'text-blue-gray-A50 '} text-xxs`}>{slugFormatter(value)}</h1>
+//               <div className={`${location.pathname.split('/').length - 2 === i ? 'hidden' : ''} w-[18px] h-[18px] flex justify-center items-center`}>
+//                 <img className='w-[5px] h-[8px]' src={Icons.ChevronRight} alt="" />
+//               </div>
+//             </div></Link>
+//           ))
+//         }
+//       </div>
+//       <div className="flex items-center">
+//         <img src={Icons.admin} alt="user1" className="w-6 h-6 mr-2" />
+//         <div>
+//           <h4 className="text-xxs text-ct-blue-95 font-medium mb-0.5">
+//             Md. Jalal Uddin
+//           </h4>
+//           <h5 className="text-xxs text-blue-gray-75 mb-0">Admin</h5>
+//         </div>
+//         <button className="ml-5">
+//           <img src={Icons.arrow_drop_down_blue_gray} alt="user1" />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Secondary;
