c2189bfc2c897eeaa9dc5de051232fd4ef80f240
jalal.seu.cse <jalal.seu.cse@gmail.com>
2023-02-28 17:12:21 +0600

    edit speech status done sort disign done

--- src/components/Table/Type11.tsx
+++ src/components/Table/Type11.tsx
@@ -160,22 +160,21 @@ const Type11 = ({ data }: Props) => {
 
         filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }: any) => (
 
-            <div onKeyDown={(e) => e.stopPropagation()} className="w-[260px] border-[1px] border-blue-gray-30 rounded-[8px] ">
+            <div onKeyDown={(e) => e.stopPropagation()} className="w-[260px] -mr-[150px] -mt-[50px]  rounded-[8px]" >
                 {
                     uploadStatus?.map((item: string, i: number) => (
-                        <div onClick={() => item === AUDIO_FILE_UPLOADED ? onUploadStatus(AUDIO_FILE_UPLOADED) : onUploadStatus(AUDIO_FILE_FAILED)} className={`${(isUploaded && item === AUDIO_FILE_UPLOADED) ? 'bg-green-10' : (isFailed && item === AUDIO_FILE_FAILED) ? 'bg-venetian-red' : ''} py-4 pl-4 pr-3 flex items-center justify-between`} key={i}>
+                        <div onClick={() => item === AUDIO_FILE_UPLOADED ? onUploadStatus(AUDIO_FILE_UPLOADED) : onUploadStatus(AUDIO_FILE_FAILED)} className={`${(isUploaded && item === AUDIO_FILE_UPLOADED) ? 'bg-green-10' : (isFailed && item === AUDIO_FILE_FAILED) ? 'bg-venetian-red' : 'bg-white'} h-[48px] py-4 pl-4 pr-3 flex items-center justify-between ${item === AUDIO_FILE_FAILED ? 'rounded-[8px] border-[1px] rounded-t-none border-t-transparent border-blue-gray-30' : 'rounded-[8px] border-[1px] rounded-b-none border-b-transparent border-blue-gray-30 '}`} key={i}>
                             <div className='flex items-center gap-x-3'>
                                 <div className={`${(item === AUDIO_FILE_UPLOADED) ? 'bg-secondary-green-50' : (item === AUDIO_FILE_FAILED) ? 'bg-secondary-red-50' : ''} w-[9px] h-[9px] rounded-[50%] `} />
                                 <h1 className='text-green-60 text-sm font-medium'>{item}</h1>
                             </div>
                             {
                                 ((isUploaded && item === AUDIO_FILE_UPLOADED) || (isFailed && item === AUDIO_FILE_FAILED)) &&
-                                <img className='w-[40px] h-[20px]' src={Icons.check_green} alt="" />
+                                <img className='h-6 w-6'  src={Icons.check_green} alt="" />
                             }
                         </div>
                     ))
                 }
-
             </div>
         ),
         filterIcon: (filtered: boolean) => (
