efbf430657a74aa64af49bf81402a258e5d9a1f2
chandan1001 <chandan15-11611@diu.edu.bd>
2023-03-20 12:37:22 +0600

    ui: add back navigation in speech page

--- src/components/containers/userManagement/Speeches/SpeechHeader/index.tsx
+++ src/components/containers/userManagement/Speeches/SpeechHeader/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Navigator } from '../../../../Navigator';
+import { useParams, useLocation } from 'react-router-dom';
 
 const SpeechHeader = () => {
+  const { id, sId } = useParams<{ id: string, sId: string }>();
+  const location = useLocation();
+  console.log("pathssdfdfd", location.pathname.split("user-management/speeches")[0])
   return (
-    <div>SpeechHeader</div>
+    <div>
+      <Navigator.Back path={`${location.pathname.split("user-management/speeches")[0]}`} title={`Speech Upload, Id: ${sId?.substring(0, 7)}`} />
+    </div>
   )
 }
 
