7f35f4b8f473037f32026c35f07bbfecf2d5336a
SRaihan <syed.hasan.meem@gmail.com>
2023-04-06 15:11:45 +0600

    refeat-fixed audio management module

---
+++ src/pages/AudioManagement/UploadAudio/Annotation/AnnotatedFilesUpload.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AnnotatedFilesContainer from '../../../../components/containers/AudioManagement/Annotation/AnnotatedFiles';
+
+const AnnotatedFilesUpload = () => {
+    return (
+        <div className='pl-[26px]'>
+            <AnnotatedFilesContainer />
+        </div>
+    );
+};
+
+export default AnnotatedFilesUpload;
