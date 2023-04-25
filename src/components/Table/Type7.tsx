329308aabb0afd04e65369b4622803389ded4784
SRaihan <syed.hasan.meem@gmail.com>
2023-04-25 16:16:18 +0600

    fix: pending table's remark icon size increased

--- src/components/Table/Type7.tsx
+++ src/components/Table/Type7.tsx
@@ -154,7 +154,7 @@ const Type7 = ({ data }: Props) => {
                     }}>
                     <button className='flex justify-center items-center w-9 h-9 rounded-full transition ease-out duration-300 hover:bg-blue-gray-20 active:border active:border-blue-gray-A10'>
                         <img
-                            src={Icons.File} className="h-[15px] w-[12px] cursor-pointer"
+                            src={Icons.File} className="h-4 w-4 cursor-pointer"
                             alt=""
                         />
                     </button>
